3764f62a854eb720fe5b527e3ec3830c27a61fcf
Carlos Zinato <carlosh.lz@hotmail.com>
2024-10-17 21:45:13 +0200

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Carlos Zinato on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
