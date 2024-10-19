85790668cbc87985d62dcfed3e5f2360edf48f2d
Carlos Zinato <carlosh.lz@hotmail.com>
2024-10-20 00:41:08 +0200

    transform NewBook view into navigationView

--- ReadMe/Views/NewBookView.swift
+++ ReadMe/Views/NewBookView.swift
@@ -6,20 +6,30 @@
 //
 
 import SwiftUI
-import class PhotosUI.PHPickerViewController
 
 struct NewBookView: View {
     @ObservedObject var book = Book(title: "", author: "")
     @State var image: Image? = nil
 
     var body: some View {
-        VStack(spacing: 24) {
-            TextField("Book Title", text: $book.title)
-                .font(.title)
-            TextField("Book Author", text: $book.author)
-                .font(.title2)
-                .foregroundStyle(.secondary)
-            ReviewAndImageStack(book: book, image: $image)
+        NavigationView {
+            VStack(spacing: 24) {
+                TextField("Title", text: $book.title)
+                    .font(.title)
+                TextField("Author", text: $book.author)
+                    .font(.title2)
+                    .foregroundStyle(.secondary)
+                ReviewAndImageStack(book: book, image: $image)
+            }
+            .navigationTitle("New book")
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .status) {
+                    Button("Save") {
+                        print("add new book")
+                    }
+                }
+            }
         }
     }
 }
